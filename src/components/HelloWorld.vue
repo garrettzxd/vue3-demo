1721978d88f1f57d86f6728a8e8811d2a9edda40
zhangxd002 <zhangxd002@lianlianpay.com>
2024-10-30 09:36:03 +0800

    chore: init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <h1>{{ props.msg }}</h1>
+    <button @click="visibleControl">show modal</button>
+    <modal-test ref="confirm" :visible="visible" :confirm="confirm" />
+  </div>
+</template>
+
+<script setup>
+import {defineOptions,defineProps,ref} from "vue";
+import ModalTest from './ModalTest.vue';
+
+defineOptions({name: 'App'})
+const props = defineProps({
+  msg: String
+})
+const visible = ref(false)
+
+const visibleControl = () => {
+  visible.value = true;
+}
+
+const confirm = () => {
+  console.log('confirm')
+}
+</script>
+
+<!--<script>-->
+<!--export default {-->
+<!--  methods: {-->
+<!--    confirm() {-->
+<!--      return new Promise((resolve) => {-->
+<!--        setTimeout(() => {-->
+<!--          resolve()-->
+<!--        }, 1000)-->
+<!--      })-->
+<!--    }-->
+<!--  },-->
+<!--}-->
+<!--</script>-->
---
+++ src/components/ModalTest.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-if="visible">
+    modal test
+    <button @click="handleConfirm">confirm</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModalTest",
+
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    },
+    confirm: Function
+  },
+
+  methods: {
+    async handleConfirm() {
+      if (this.confirm) {
+        this.confirm()
+      }
+    }
+  }
+}
+</script>
